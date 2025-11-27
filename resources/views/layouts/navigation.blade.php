d7240c31d228b1fe4b4df082b3501a638a46d6ee
Felipe Andres <felipevsconcelos@gmail.com>
2025-11-27 19:37:18 +0000

    style/remove laravel logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('contacts.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <span class="block w-auto fill-current text-gray-800">Alfasoft</span>
                     </a>
                 </div>
 
